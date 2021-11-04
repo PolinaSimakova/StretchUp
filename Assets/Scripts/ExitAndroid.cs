56adf4f190d437a8e09106d513c986db3b5ea4f1
Polina Simakova <simakovapolina96@gmail.com>
2021-11-04 22:03:57 +0300

    adding of black bars and exit from aplication

--- Assets/Scripts/ExitAndroid.cs
+++ Assets/Scripts/ExitAndroid.cs
@@ -8,11 +8,16 @@ public class ExitAndroid : MonoBehaviour {
         if (Application.platform == RuntimePlatform.Android ||
             Application.platform == RuntimePlatform.WindowsEditor) {
             if (Input.GetKeyDown(KeyCode.Escape)) {
-                //Debug.Log(SceneManager.GetActiveScene().name);
                 if (SceneManager.GetActiveScene().name == "Warm-upVideo") {
                     SceneManager.LoadScene("Warm-up"); // загружаете нужную сцену
                 }else if (SceneManager.GetActiveScene().name == "WorkoutNew") {
                     SceneManager.LoadScene("Workouts"); // загружаете нужную сцену
+                }else if (SceneManager.GetActiveScene().name == "Home") {
+                    Application.Quit();
+                }else if (SceneManager.GetActiveScene().name == "Warm-up") {
+                    Application.Quit();
+                }else if (SceneManager.GetActiveScene().name == "Workouts") {
+                    Application.Quit();
                 }
             }
         }
