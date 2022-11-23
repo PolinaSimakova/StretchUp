6aedff62469e8ecdca7e466da85da4c6abcd5cbd
Polina Simakova <simakovapolina96@gmail.com>
2022-11-23 17:12:43 +0300

    реклама для побликации андройд

--- Assets/Scripts/NewScripts/AdBanner.cs
+++ Assets/Scripts/NewScripts/AdBanner.cs
@@ -1,30 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using UnityEngine.SceneManagement;
 
-public class AdBanner : MonoBehaviour {
-    private string UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
 
-    private BannerView banner;
-    private void OnEnable() {
+public class AdBanner : MonoBehaviour
+{
+    private BannerView bannerView;
+    private void Start() {
+        MobileAds.Initialize(status => { });
+        RequestBanner();
+    }
+    
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+       // string adUnitIdBannerTest = "ca-app-pub-3940256099942544/6300978111";
+        string adUnitIdBanner = "ca-app-pub-5684932031961846/8696513635";
+#elif UNITY_IPHONE
+            string adUnitIdBanner  = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitIdBanner  = "unexpected_platform";
+#endif
+
         if (SceneManager.GetActiveScene().name == "Workouts") {
-            banner = new BannerView(UNIT_ID, AdSize.Banner, AdPosition.Bottom);
-        }
-        else {
-            banner = new BannerView(UNIT_ID, AdSize.Banner, AdPosition.TopLeft);
-        }
-        AdRequest adRequest = new AdRequest.Builder().Build();
-        banner.LoadAd(adRequest);
-    }
 
-    public void ShowAd() {
-        banner.Show();
-    }
+            bannerView = new BannerView(adUnitIdBanner , AdSize.SmartBanner, AdPosition.Bottom);
+        } else if (SceneManager.GetActiveScene().name == "Player") {
 
-    public void Start() {
-        ShowAd();
+            bannerView = new BannerView(adUnitIdBanner ,  AdSize.Banner, AdPosition.TopLeft);
+        }
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+    }
+    
+    void OnDestroy() {
+        bannerView.Destroy();
     }
 }
